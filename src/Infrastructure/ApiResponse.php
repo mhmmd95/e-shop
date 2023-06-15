61946d3e0836b361cbb921c3d39ee6427c029c6d
mhmmd95 <mohammadalabras77@gmail.com>
2023-06-15 14:04:57 +0300

    added: autoload psr-4 Infrastructure namespace

---
+++ src/Infrastructure/ApiResponse.php
@@ -0,0 +1,30 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Infrastructure;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\MessageBag;
+use JustSteveKing\StatusCode\Http;
+
+trait ApiResponse {
+
+    private function handle_success(array|string|null $data, string $message = 'sucess', int $code = Http::OK->value): JsonResponse {
+
+        return response()->json([
+            'result' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+
+    private function handle_error(MessageBag|null $errors, string $message = 'error', int $code = Http::BAD_REQUEST->value): JsonResponse {
+
+        return response()->json([
+            'result' => false,
+            'message' => $message,
+            'errors' => $errors,
+        ], $code);
+    }
+}
