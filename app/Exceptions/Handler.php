cdd690c09fd4230dc94de76aeacdaed3bb3ac6fc
mhmmd95 <mohammadalabras77@gmail.com>
2023-06-15 14:01:38 +0300

    created:custom exception for password login edited: Laravel handler class

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,30 +1,53 @@
 <?php
 
+declare (strict_types = 1);
+
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Infrastructure\ApiResponse;
+use JustSteveKing\StatusCode\Http;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
-class Handler extends ExceptionHandler
+final class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
+    use ApiResponse;
+
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        // Check for a specific request header
+        $checkAppJSONHeader = $request->header('Accept');
+
+        if ($checkAppJSONHeader === 'application/json') {
+            // Override the exception handling behavior based on the header value
+
+            if( $e instanceof ModelNotFoundException)
+                return $this->handle_error(null, 'Can\'t find your record!');
+
+            if( $e instanceof NotFoundHttpException)
+                return $this->handle_error(null, 'Wrong url, not found');
+
+            if( $e instanceof AuthenticationException)
+                return $this->handle_error(null, $e->getMessage(), Http::UNAUTHORIZED->value);
+        }
+
+        // Default exception rendering behavior
+        return parent::render($request, $e);
     }
 }
